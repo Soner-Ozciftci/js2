50f7cacaa698938e9cd5777ece0aa084f3324c08
Soner <ozciftci@gmail.com>
2023-05-25 02:06:16 +0200

    first commit

---
+++ operator.js
@@ -0,0 +1,66 @@
+console.log("JS dosyası bağlandı.");
+
+var deneme = 5;
+varDeneme = 6;
+
+let myVar = 5;
+
+const myVar1 = 6;
+
+// Aritmetiksel Operatörler
+// +,-,*,/,%
+
+const num1 = 10;
+const num2 = 69;
+
+const toplam = num1 + num2;
+
+console.log(toplam);
+
+const msg1 = "Hello";
+const msg2 = "World!";
+
+const mesaj = msg1 + msg2;
+console.log(mesaj);
+
+const firstName = "SONER";
+const lastName = "OZCIFTCI";
+console.log(firstName, lastName);
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// `merhaba` `${değişken} ${değişken2}`;
+
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName2);
+
+const num3 = "6";
+const num4 = "7";
+console.log(num3 + num4);
+console.log(num3 + 7);
+
+console.log(7 + num3);
+console.log(typeof (7 + num3)); // string
+
+const x = 55;
+const y = 44;
+
+console.log(x - y);
+
+const myNum = "1";
+console.log(x - myNum);
+
+const yourNum = "one";
+console.log(x - yourNum);
+
+console.log(x - "a");
+
+let carp1 = 7;
+let carp2 = 9;
+console.log(carp1 * carp2);
+
+console.log("a" * "b");
+console.log("a" * 3);
+
+console.log("5" * "9");
+console.log(typeof ("5" * "9"));
